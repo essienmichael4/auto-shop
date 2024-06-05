2da9fa7ca67c46fc700414c9f97eef64b716e398
Code_Junior <essienmichael4@gmail.com>
2024-06-05 18:27:17 +0000

    Login and create new user backend

--- autoshop-be/src/controllers/user.controller.ts
+++ autoshop-be/src/controllers/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import log from "../utils/logger";
+import * as bcrypt from 'bcrypt'
+import { createNewUser } from "../services/user.service";
 
 /**
  * 
@@ -9,9 +11,18 @@ import log from "../utils/logger";
  */
 export const createUser = async (req:Request, res:Response)=>{
     try{
-        
+        const {firstname, lastname, othernames, email, role, departments, password} = req.body
+
+        let hashedPassword:string = ""
+        if(password){
+            hashedPassword = await bcrypt.hash(password, 10)
+        }
+
+        const newUser = createNewUser(firstname, lastname, email, role, departments, othernames, hashedPassword) 
+
+        res.send({newUser, message: "User created successfully"})
+
     }catch(err:any){
-        
-        
+        res.status(400).json(err)
     }
 }
--- autoshop-be/src/services/user.service.ts
+++ autoshop-be/src/services/user.service.ts
@@ -1,1 +1,32 @@
+import { PrismaClient, Role } from '@prisma/client'
+const prisma = new PrismaClient()
 
+export const findUserByEmail = (email:string)=>{
+    return prisma.user.findUnique({where:{email}, include: {departments:true}})
+}
+
+export const findUserById = (id:number)=>{
+    return prisma.user.findUnique({where:{id}})
+}
+
+export const createNewUser = (firstname:string, lastname:string, email:string, role: Role, departments: {office: string}[], othernames?:string, password?:string)=>{
+    return prisma.user.create({data: {
+        firstname,
+        lastname,
+        othernames,
+        password,
+        email,
+        status: "Active",
+        role,
+        departments: {
+            connectOrCreate: departments.map(department => {
+                return {
+                    where: { office : department.office},
+                    create: { office :department.office}
+                };
+            })
+        }
+    }
+            
+    })
+}
