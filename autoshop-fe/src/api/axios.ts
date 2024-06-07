1957282f280561fc80ebc35c9ec9cb97fbbe7b2e
Code_Junior <essienmichael4@gmail.com>
2024-06-07 10:04:05 +0000

    Clean-up

--- autoshop-fe/src/api/axios.ts
+++ autoshop-fe/src/api/axios.ts
@@ -1,9 +1,5 @@
 import axios, { AxiosInstance } from "axios";
 
-// export default axios.create({
-//     baseURL: "http://localhost:5000/api/v1"
-// })
-
 export const axios_instance : AxiosInstance = axios.create({
     baseURL: "http://localhost:5000/api/v1"
 });
