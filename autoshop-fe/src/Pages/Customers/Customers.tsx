999ee23f586667e63d13c0629e27c2d684a47666
Code_Junior <essienmichael4@gmail.com>
2024-06-08 16:57:43 +0000

    Create customer api

--- autoshop-fe/src/Pages/Customers/Customers.tsx
+++ autoshop-fe/src/Pages/Customers/Customers.tsx
@@ -1,17 +1,10 @@
 import Header from '@/components/Header/Header'
-import useAuth from '@/hooks/useAuth'
-import React, { useState } from 'react'
+// import useAuth from '@/hooks/useAuth'
 import customers from "../../assets/dashboard.jpg"
-import { differenceInDays, startOfMonth } from 'date-fns'
-import { DateRangePicker } from '@/components/ui/date-range-picker'
-import { toast } from 'sonner'
+import CreateNewCustomerDialog from '@/components/CreateNewCustomerDialog/CreateNewCustomerDialog'
 
 const Customers = () => {
-    const {auth} = useAuth()
-    const [dateRange, setDateRange] = useState<{from: Date, to: Date}>({
-        from: startOfMonth(new Date()),
-        to: new Date()
-    })
+    // const {auth} = useAuth()
 
   return (
     <div>
@@ -23,7 +16,10 @@ const Customers = () => {
             <div className='flex items-center justify-between gap-8'>
                 <h2 className='text-2xl text-white font-semibold'>Customers</h2>
                 <div className='flex gap-4'>
-                    <button className='bg-white px-4 py-2 rounded'>Add Customer</button>
+                    <CreateNewCustomerDialog trigger={
+                        <button className='bg-white px-4 py-2 rounded'>Add Customer</button>
+
+                    } />
                 </div>
             </div>
             <div className='p-4 bg-white/15 mt-4 rounded-xl backdrop-blur-sm'>
--- autoshop-fe/src/main.tsx
+++ autoshop-fe/src/main.tsx
@@ -5,14 +5,17 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AuthProvider from './context/authContext.tsx'
 import { Toaster } from "@/components/ui/sonner";
+import ReactQueryProvider from './components/ReactQueryProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Toaster richColors position="bottom-right" />
     <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+      <ReactQueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ReactQueryProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
