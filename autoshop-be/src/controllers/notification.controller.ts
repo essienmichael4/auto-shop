57437b58320407e2347d29d2ef4265009110c3e7
Code_Junior <essienmichael4@gmail.com>
2024-06-12 17:36:16 +0000

    Notification api and emails

---
+++ autoshop-be/src/controllers/notification.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import log from "../utils/logger";
+import { AuthRequest } from "../types/authRequest.type";
+import { getAllServices } from "../services/service.service";
+import { createNotification, getAllNotifications, sendEmail } from "../services/notification.service";
+
+export const getNotifications = async (req:Request, res:Response)=>{
+    const notifications = await getAllNotifications()
+    res.send(notifications)
+}
+
+export const scheduleNotificationAndSendMails = async()=>{
+    const services = await getAllServices()
+
+    const due = services.filter(service=>{
+        const millisecondsDiff = (new Date(service.dueDate)).valueOf() - (Date.now()).valueOf()
+        const duration = Math.round(
+            millisecondsDiff / (1000 * 3600 * 24)
+        )
+        
+        if (duration >= 0 && duration <=2){
+            return service
+        }
+    })
+        
+    if(!due) return
+
+    await createNotification(due)
+
+    await sendEmail(due)
+}
+
+
--- autoshop-be/src/index.ts
+++ autoshop-be/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import cors from 'cors'
 import dotenv from 'dotenv'
 import log from "./utils/logger"
+import cron from "node-cron"
 
 //Routes
 import userRoute from "./routes/user.route"
 import authRoute from "./routes/auth.route"
 import customerRoute from "./routes/customer.route"
 import serviceRoute from "./routes/service.route"
+import notificationRoute from "./routes/notification.route"
+import { scheduleNotificationAndSendMails } from "./controllers/notification.controller";
 
 const port = process.env.PORT || 5001
 const app = express()
@@ -19,8 +22,15 @@ app.use("/api/v1", authRoute)
 app.use("/api/v1", userRoute)
 app.use("/api/v1", customerRoute)
 app.use("/api/v1", serviceRoute)
+app.use("/api/v1", notificationRoute)
 
 app.listen(port, ()=>{
     log.info(`Server running on port ${port}`);   
     
+})
+
+cron.schedule("0 6 * * *", ()=>{
+    console.log("------------------------")
+    console.log("Running task")
+    scheduleNotificationAndSendMails()
 })
